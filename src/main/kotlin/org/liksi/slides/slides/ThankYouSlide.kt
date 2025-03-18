5b613b2354ac3535dea64c9c887f76ebc31831c5
DURAND, Mathieu <prestataire.0169783@ouest-france.fr>
2025-03-18 23:04:37 +0100

    ✨ Add repository links to Thank You slide and style them

--- src/main/kotlin/org/liksi/slides/slides/ThankYouSlide.kt
+++ src/main/kotlin/org/liksi/slides/slides/ThankYouSlide.kt
@@ -8,7 +8,7 @@ import kotlinx.html.*
 class ThankYouSlide : Slide {
     override fun FlowContent.render() {
         div(classes = "slide content-slide thank-you-slide") {
-            img(src = "/static/images/octopus-icon.svg", classes = "octopus-icon octopus-center")
+            img(src = "/static/images/octopus-icon.svg", classes = "octopus-icon octopus-bottom-right")
             
             h2 {
                 +"Merci pour votre attention !"
@@ -17,6 +17,18 @@ class ThankYouSlide : Slide {
             div(classes = "content centered") {
                 h3 {
                     +"Des questions ?"
+                }
+            }
+            
+            div(classes = "links-container bottom-links") {
+                a(href = "https://github.com/NoxFr/kotlin-htmx", classes = "repo-link") {
+                    img(src = "/static/images/github-icon.svg", classes = "github-icon")
+                    +"Le code source"
+                }
+                
+                a(href = "https://github.com/NoxFr/kotlin-htmx-slides", classes = "repo-link") {
+                    img(src = "/static/images/github-icon.svg", classes = "github-icon")
+                    +"Les slides"
                 }
             }
         }
